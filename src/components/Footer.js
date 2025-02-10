37fe5f5b7fa21f81651ea0f95e700d5581c1fdb1
Mariana <mariana.placito@gmail.com>
2025-02-10 18:38:50 +0000

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import '../app/styles/footer.css';
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p>&copy; 2025 My Blog. All rights reserved.</p>
+    </div>
+  );
+}
