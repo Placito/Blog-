da1668821013965fcbf47a5e1cfe64285491e292
Mariana <mariana.placito@gmail.com>
2025-02-17 15:23:07 +0000

    transform the footer stylçes in Tailwind CSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Footer() {
   
   return (
-    <div className="footer">
-      <p>&copy; 2025 My Blog. All rights reserved.</p>
+    <div className="footer bg-gray-800 text-white p-5 text-center fixed bottom-0 left-0 w-full">
+      <p >&copy; 2025 My Blog. All rights reserved.</p>
     </div>
   );
 }
