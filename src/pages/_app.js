ada9a28c5966c5060a75f3e4796b028082b1e450
Mariana <mariana.placito@gmail.com>
2025-02-17 12:43:32 +0000

    add a title for the document

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,16 @@ import React from "react";
 import "./styles/globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>My Awesome Blog</title>
+        <meta name="description" content="A blog about awesome things!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
